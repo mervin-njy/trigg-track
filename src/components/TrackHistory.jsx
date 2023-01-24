50d7fd081e4ca472efbe259d31408f3d58e7b7d3
mervin-njy <mervin.njy@outlook.com>
2023-01-24 17:20:27 +0800

    TrackHistory component w/ display upon selection

--- src/components/TrackHistory.jsx
+++ src/components/TrackHistory.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import loadingSpinner from "./LoadingSpinner";
 
 const Display = (props) => {
-  const { diet, userID } = props.variable;
-  console.log(diet);
-  //   if (props.key === userID)
+  const [selection, setSelection] = useState("");
+  const [display, setDisplay] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDisplay = async (url, signal) => {
+    setIsLoading(true);
+    setError(null);
+    setDisplay(null);
+
+    try {
+      const rest = await fetch(url, { signal });
+
+      const data = await rest.json();
+
+      setDisplay({
+        // display data from firebase rtdb
+      });
+    } catch (err) {
+      if (err.name !== "AbortError") {
+        setError(err.message);
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleSelectionChange = (event) => {
+    setSelection(event.target.value);
+  };
+
+  useEffect(() => {
+    const url = `https://healthlogger-a9842-default-rtdb.firebaseio.com/users/${selection}.json`;
+
+    const controller = new AbortController();
+    fetchDisplay(url, controller.signal);
+
+    return () => {
+      controller.abort();
+    };
+  }, [selection]);
+
   return (
-    <div className="variable-container">
-      {/* <h3>This is your diet on</h3> */}
-      <div className="variable-title">
-        <h4>Breakfast:</h4>
+    <>
+      <section>
+        <h2>Select Display Group:</h2>
+        <div className="selection-container">
+          <select id="selection" onChange={handleSelectionChange}>
+            <option value="conditions">conditions</option>
+            <option value="variables">variables</option>
+          </select>
+        </div>
+      </section>
+
+      <br />
+      
+      <div className="variable-container">
+        <div className="variable-title"></div>
+        <ul className="variable-descriptions"></ul>
       </div>
-      <ul className="variable-descriptions">
-        {diet.breakfast.items.map((item, index) => (
-          <li>
-            item {index}: {item}
-          </li>
-        ))}
-        <li>location: {diet.breakfast.location}</li>
-        <li>rating: {diet.breakfast.rating}</li>
-      </ul>
-    </div>
+    </>
   );
 };
 
